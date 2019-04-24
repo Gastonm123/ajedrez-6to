c0b04337ed2c9d1fc6631e208d1a86cb66eeef37
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-04-24 00:26:10 -0300

    Añadido test inicial para caballo

--- TestCaballo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require("Caballo.php");
-
-$caballito = new Caballo("d2");
-var_dump($caballito->movimientosPosibles());
-
-$caballito->posicionarEn("d4");
-var_dump($caballito->movimientosPosibles());
